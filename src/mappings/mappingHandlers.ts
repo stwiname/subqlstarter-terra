21d53cd231f3a3e236628dbe92a817e3cfceaa79
Naveen V <velnaveen99@gmail.com>
2022-03-17 22:41:01 +0530

    support latest changes in terra interface

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,17 +1,43 @@
-import {StarterEntity} from "../types";
+import { Transfer, MsgSendEntity} from "../types";
 import { hashToHex } from '@terra-money/terra.js/dist/util';
-import { TerraEvent, TerraBlock } from '@subql/types-terra';
+import { TerraEvent, TerraBlock, TerraMessage, TerraTransaction } from '@subql/types-terra';
+import { MsgSend } from "@terra-money/terra.js";
 
-export async function handleBlock(block: TerraBlock): Promise<void> {
-    let record = new StarterEntity(block.block.block.header.height);
-    record.hash = hashToHex(block.block.block_id.hash);
-    await record.save();
+
+export async function handleBlock(block: TerraBlock) {
+
 }
 
-export async function handleEvent(event: TerraEvent): Promise<void> {
-    const record = await StarterEntity.get(event.block.block.header.height);
-    record.sender = event.event['transfer']['sender'];
-    record.recipient = event.event['transfer']['recipient'];
-    record.amount = event.event['transfer']['amount'];
-    await record.save();
+export async function handleTransaction(tx: TerraTransaction) {
+
+}
+
+export async function handleMessage(msg: TerraMessage) {
+    const idx = `${msg.tx.tx.txhash}-${msg.idx}`;
+    const msgObj = msg.msg.toData() as MsgSend.Data;
+    const msgSendEntity = new MsgSendEntity(idx);
+    msgSendEntity.amount = JSON.stringify(msgObj.amount);
+    msgSendEntity.toAddress = msgObj.to_address;
+    msgSendEntity.fromAddress = msgObj.from_address;
+    await msgSendEntity.save();
+}
+
+export async function handleEvent(event: TerraEvent) {
+    const idx = `${event.tx.tx.txhash}-${event.msg.idx}-${event.idx}`;
+    const transfer = new Transfer(idx);
+    for(const attr of event.event.attributes) {
+        switch(attr.key) {
+            case 'sender':
+                transfer.sender = attr.value;
+                break;
+            case 'recipient':
+                transfer.recipient = attr.value;
+                break;
+            case 'amount':
+                transfer.amount = attr.value;
+                break;
+            default:
+        }
+    }
+    await transfer.save();
 }
